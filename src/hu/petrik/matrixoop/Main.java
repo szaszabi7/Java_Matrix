2decf3ef87607b47cb464a216e0d7bc70d5e78a3
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-03 15:26:07 +0100

    sum, db, max

--- src/hu/petrik/matrixoop/Main.java
+++ src/hu/petrik/matrixoop/Main.java
@@ -6,5 +6,9 @@ public class Main {
         Matrix m = new Matrix();
         System.out.println("Néhyzetes-e: " + (m.isNegyzetes()? "igen": "nem"));
         System.out.println(m);
+        System.out.println("Mátrix mérete: " + m.getSorokSzama() + "x" + m.getOszlopokSzama());
+        System.out.println("Mátrix elemeinek összege: " + m.osszeg());
+        System.out.println("Mátrix páros elemeinek száma: " + m.parosDb());
+        System.out.println("Mátrix legnagyobb eleme: " + m.max());
     }
 }
--- src/hu/petrik/matrixoop/Matrix.java
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -23,9 +23,56 @@ public class Matrix {
         }
     }
 
+    public int getSorokSzama() {
+        return sorokSzama;
+    }
+
+    public int getOszlopokSzama() {
+        return oszlopokSzama;
+    }
+
     public boolean isNegyzetes() {
         return this.sorokSzama == this.oszlopokSzama;
     }
+
+    public long osszeg() {
+        long sum = 0;
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                sum += this.matrix[i][j];
+            }
+        }
+
+        return sum;
+    }
+
+    public int parosDb() {
+        int db = 0;
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                if (this.matrix[i][j] % 2 == 0) db++;
+            }
+        }
+
+        return db;
+    }
+
+    public int max() {
+        int max = this.matrix[0][0];
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                if (this.matrix[i][j] > max) {
+                    max = this.matrix[i][j];
+                }
+            }
+        }
+
+        return max;
+    }
+
 
     @Override
     public String toString() {
