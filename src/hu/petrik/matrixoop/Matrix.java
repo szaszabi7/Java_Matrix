e4f999e9f95dd1bfcc6c08ce1e5346785b6b9b86
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-03 14:59:28 +0100

    init

---
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -0,0 +1,39 @@
+package hu.petrik.matrixoop;
+
+public class Matrix {
+    private int sorokSzama;
+    private int oszlopokSzama;
+    private int[][] matrix;
+
+    public Matrix(int sorokSzama, int oszlopokSzama, int[][] matrix) {
+        this.sorokSzama = sorokSzama;
+        this.oszlopokSzama = oszlopokSzama;
+        this.matrix = matrix;
+    }
+
+    public Matrix() {
+        this.sorokSzama = (int)(Math.random()*10)+1;
+        this.oszlopokSzama = (int)(Math.random()*10)+1;
+        this.matrix = new int[this.sorokSzama][this.oszlopokSzama];
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                this.matrix[i][j] = (int)(Math.random()*90)+10;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                s += matrix[i][j] + " ";
+            }
+            s += "\n";
+        }
+
+        return s;
+    }
+}
